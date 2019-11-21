2250bcc264281b21c1a7f46e1525d23e9906b166
Brandon Lam <43765477+Lammmmmm@users.noreply.github.com>
2019-11-21 13:00:49 -0500

    Committing app.js file

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,76 +1,201 @@
-// TODO: store checkbox as boolean instead of string.
-
-
-const cafeList = document.querySelector('#garage-sales');
+// const saleList = document.querySelector('#garage-sales');
 const form = document.querySelector('#add-sale-form');
+const sale = document.querySelector('#search-sale-form');
 
-// create element and render sale listing
-function renderSale(doc)
+// Saving Data
+if (form)
 {
-    let li = document.createElement('li');
-    let Address = document.createElement('span');
-    let Description = document.createElement('span');
-    let Electronics = document.createElement('span');
-    let Cross = document.createElement('div');
+    form.addEventListener('submit', async (e) => 
+    {
+        e.preventDefault();
+        if (form.address.value == '' || form.date.value == '')
+        {
+            return;
+        }
+        await db.collection('Sellers').add
+        ({
+            address: form.address.value,
+            date: form.date.valueAsDate,
+            electronics: form.electronics.checked,
+            furniture: form.furniture.checked,
+            art: form.art.checked,
+            clothing: form.clothing.checked,
+            books: form.books.checked,
+            bicycles: form.bicycles.checked,
+            jewelry: form.jewelry.checked,
+            toys: form.toys.checked, 
+            other: form.other.checked
+        }).then(ref => 
+        {
+            console.log('Added document with ID: ', ref.id);
+        });
 
-    li.setAttribute('data-id', doc.id);
-    Address.textContent = doc.data().Address;
-    Description.textContent = doc.data().Description;
-    Electronics.nodeValue = doc.data().Electronics;
-    var temp = doc.data().Electronics;
-    //document.querySelector('.messageCheckbox').checked;
-    Cross.textContent = 'x';
+        form.address.value = '';
+        form.date.value = '';
+        form.electronics.checked = false;
+        form.furniture.checked = false;
+        form.art.checked = false;
+        form.clothing.checked = false;
+        form.bicycles.checked = false;
+        form.jewelry.checked = false;
+        form.toys.checked = false;
+        form.other.checked = false;
 
-    li.appendChild(Address);
-    li.appendChild(Description);
-    li.appendChild(Electronics);
-    li.appendChild(Cross);
+        location.href = "SellerTerminal.html";        
+    });    
+}
 
-    cafeList.appendChild(li);
 
-    // Deleting Data
-    Cross.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('Sellers').doc(id).delete();
+if (sale)
+{
+    sale.addEventListener('submit', async (e) => 
+    {
+        e.preventDefault();
+        var gsale = sale.elements;
+        let sales = new Set([]);
+        var len = sale.elements.length - 1;
+        var element;
+        var date;
+
+        // Add a check for blank address and date
+        // have to make another check if date matches date given before throwing it into list
+        // need to fix input time as well with the date
+        // need to check for duplicates
+
+        date = gsale[1];
+
+        for (var i = 0; i < len; i++)
+        {
+            element = gsale[i]
+            if (element.type == "checkbox" && element.checked === true)
+            {
+                await db.collection('Sellers').where(element.id, '==', true).where(date.id, '==', date.valueAsDate).get()
+                .then(snapshot => {
+                    if (snapshot.empty) {
+                        console.log("No matching documents");
+                        return;
+                    }
+                    snapshot.forEach(doc => {
+                        sales.add(doc.data().address);
+                        console.log(doc.id, "=>", doc.data());
+                    });
+                })
+                .catch(err => {
+                    console.log("Error getting documents", err);
+                });
+
+                var arrlen = sales.size;
+
+                console.log("arrlen before =", arrlen);
+
+                console.log(sales);
+            }
+        }
+
+        // prevent empty strings for date and address, or no boxes checked
+        // check for match date and categories.
+    
+        location.href = "SailorTerminal.html";
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function initMap()
+{
+    var searchBox = new google.maps.places.SearchBox(document.getElementById('address'));
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
     })
 }
 
-// Getting Data
-// db.collection('Sellers').get().then( (snapshot) => {
-//     snapshot.docs.forEach(doc => {
-//         renderSale(doc);
+
+// // Real-Time Listener 
+// db.collection('Sellers').onSnapshot(snapshot => {
+//     let changes = snapshot.docChanges();
+//     changes.forEach(change => {
+//         if (change.type == 'added')
+//         {
+//             renderSale(change.doc);
+//         }
+//         else if (change.type == 'removed')
+//         {
+//             let li = saleList.querySelector('[data-id=' + change.doc.id + ']');
+//             saleList.removeChild(li);
+//         }
+        
 //     })
+
 // })
 
-// Saving Data
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('Sellers').add({
-        Address: form.Address.value,
-        Description: form.Description.value,
-        // Create a collection of categories with booleans.
-        Electronics: form.Electronics.value
-    });
-    form.Address.value = '';
-    form.Description.value = '';
-    form.Electronics.value = '';
-});
 
-// Real-Time Listener 
-db.collection('Sellers').onSnapshot(snapshot => {
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-        if (change.type == 'added')
-        {
-            renderSale(change.doc);
-        }
-        else if (change.type == 'removed')
-        {
-            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
-            cafeList.removeChild(li);
-        }
-        
-    })
+// Either put all information entries under one form or create 
+// link all different form id's under one event listener 
 
-})
+// Instead of address field i have, set it up for the query
+
+
+
+
+// // create element and render/display sale
+// function renderSale(doc)
+// {
+//     let li = document.createElement('li');
+//     let Address = document.createElement('span');
+//     let Description = document.createElement('span');
+//     let Cross = document.createElement('div');
+
+//     li.setAttribute('data-id', doc.id);
+    
+//     Address.textContent = doc.data().Address;
+//     Description.textContent = doc.data().Description;
+
+//     Cross.textContent = 'x';
+
+//     li.appendChild(Address);
+//     li.appendChild(Description);
+//     li.appendChild(Cross);
+
+//     saleList.appendChild(li);
+
+//     //Deleting Data
+//     Cross.addEventListener('click', (e) => {
+//         e.stopPropagation();
+//         let id = e.target.parentElement.getAttribute('data-id');
+//         db.collection('Sellers').doc(id).delete();
+//     })
+// }
